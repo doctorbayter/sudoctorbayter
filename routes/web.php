3fdfc5aa93dea9233488a5948b598a950f7c6843
Jeff Cote <web@doctorbayter.com>
2021-06-06 13:36:07 -0500

    Arreglar Usuarios

--- routes/web.php
+++ routes/web.php
@@ -146,7 +146,7 @@ Route::get('query', function(){
 
     DB::insert("INSERT INTO subscriptions (id, plan_id, user_id, created_at, updated_at) VALUES (21, '2', '2', CURRENT_TIMESTAMP, NULL)");
     
-    DB::insert("INSERT INTO fase_user (id, fase_id, user_id, created_at, updated_at) VALUES (22, '1', '2', CURRENT_TIMESTAMP, NULL)");
+    DB::insert("INSERT INTO fase_user (id, fase_id, user_id, created_at, updated_at) VALUES (21, '1', '2', CURRENT_TIMESTAMP, NULL)");
 
 
     DB::insert("INSERT INTO subscriptions (id, plan_id, user_id, created_at, updated_at) VALUES (22, '2', '2', CURRENT_TIMESTAMP, NULL)");
@@ -154,7 +154,7 @@ Route::get('query', function(){
     DB::insert("INSERT INTO fase_user (id, fase_id, user_id, created_at, updated_at) VALUES (22, '1', '2', CURRENT_TIMESTAMP, NULL)");
 
 
-    
+
 
     DB::insert("INSERT INTO day_recipe (id, day_id, recipe_id, meal, created_at, updated_at) VALUES (1, '1', '1', '1', CURRENT_TIMESTAMP, NULL), (2, '1', '2', '2', CURRENT_TIMESTAMP, NULL), (3, '1', '3', '3', CURRENT_TIMESTAMP, NULL), (4, '1', '4', '4', CURRENT_TIMESTAMP, NULL), (5, '1', '5', '4', CURRENT_TIMESTAMP, NULL)");
     

e39abbd66355ef36b4675f45013459bda972fa93
Jeff Cote <web@doctorbayter.com>
2021-07-03 15:49:36 -0500

    Agregar Dias y Semanas Fase 3

--- routes/web.php
+++ routes/web.php
@@ -220,7 +220,7 @@ Route::get('x/query', function(){
     (7, 'Secretos Fase 3', 'files/pdf/secretos-fase-3-dkp.pdf', '3', 'App\\Models\\Fase', CURRENT_TIMESTAMP, NULL)");
 */
 
- DB::statement("ALTER TABLE day_recipe MODIFY COLUMN meal ENUM('1','2','3','4','5')");
+    DB::statement("alter table day_recipe modify column meal ENUM('1','2','3','4','5') not null");
 
     echo "fin";
 

f929b07b640ab0e0e128aad9e4db57669ad6565a
Jeff Cote <web@doctorbayter.com>
2021-07-08 18:01:18 -0500

    Agregar Pagos Fase 1

--- resources/views/livewire/plan-checkout.blade.php
+++ resources/views/livewire/plan-checkout.blade.php
@@ -155,7 +155,7 @@
                                             <!-- line -->
                                             <div class="w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
                                             <!-- dot -->
-                                            <div class="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition"></div>
+                                            <div class="dot absolute w-6 h-6 bg-red-700 rounded-full shadow -left-1 -top-1 transition"></div>
                                         </div>
                                         <!-- label -->
                                         <div class="ml-3 text-gray-700 font-semibold">

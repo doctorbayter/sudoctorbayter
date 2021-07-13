647b09ecda6abb353316047027d3b371bcb05c9a
Jeff Cote <web@doctorbayter.com>
2021-07-12 22:23:18 -0500

    Nuevo material

--- app/Policies/SubscriptionPolicy.php
+++ app/Policies/SubscriptionPolicy.php
@@ -22,7 +22,7 @@ class SubscriptionPolicy
     
     public function enrolled(User $user, Subscription $subscription){
 
-        if($subscription->plan_id == 1 || $subscription->plan_id == 2) {
+        if($subscription->plan_id == 1 || $subscription->plan_id == 2 || $subscription->plan_id == 8 || $subscription->plan_id == 9) {
             if($subscription->user_id == $user->id){
                 return true;
             }

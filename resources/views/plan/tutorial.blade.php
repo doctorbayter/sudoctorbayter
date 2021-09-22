cbae410c15decd14ee650df310cbb5896a8efa1e
jeffcote044 <web@doctorbayter.com>
2021-09-22 09:03:21 -0500

    Arreglo tutorial

--- resources/views/plan/tutorial.blade.php
+++ resources/views/plan/tutorial.blade.php
@@ -8,8 +8,10 @@
         }
     @endphp
 
+
+
     <div class="flex">
-        <x-menu :fases="$user_fases" :userPlan="$user_plan" />
+        <x-menu :fases="$user_fases" :userPlan="$user_plan->id" />
         <div :class="{'w-7/12': openMenu, 'w-11/12': !openMenu}" class="bg-white  ml-auto">
 
             <header class="bg-fixed bg-cover shadow-lg" style="background-image: url({{asset('img/backgrounds/meal_plan_top_banner_2-1-1.jpg')}})">

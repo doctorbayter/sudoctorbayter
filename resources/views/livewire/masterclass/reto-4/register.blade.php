e52893d23451af5e260950d04b251cb45a99f4f2
jeffcote044 <web@doctorbayter.com>
2021-10-21 18:34:52 -0500

    Masterclass registro sin validacion de correo

--- resources/views/livewire/masterclass/reto-4/register.blade.php
+++ resources/views/livewire/masterclass/reto-4/register.blade.php
@@ -11,7 +11,8 @@
                     <h1 class="text-3xl md:text-5xl font-bold leading-none pt-2">Desinflama tu cuerpo</h1>
                     <h2 class="text-base md:text-2xl">En tan solo 4 días</h2>
                 </header>
-                <p class="my-4 text-sm md:text-base">¿Sabías que tu cuerpo puede retener hasta <b>6 litros de líquido</b> adicionales en tu cuerpo que se traduce en más peso para ti? En este reto te diré que hacer y cómo decirle adiós a esa retención de líquidos que son los que te están enfermando.</p>
+                <p class="my-4 text-sm md:text-base">¿Sabías que tu cuerpo puede retener hasta <b>6 litros de líquido adicionales</b> que se traduce en más peso para ti? En este reto te diré que hacer y cómo decirle adiós a esa retención de líquidos que son los que te están enfermando.
+                </p>
                 <div class="relative">
                     @if ($error_message)
                         <div class="px-4 py-6 bg-yellow-100 rounded-xl border border-yellow-400 text-sm">

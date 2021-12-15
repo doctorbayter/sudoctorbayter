35120897e7b38300e1529b9137b81b11006755f3
jeffcote044 <web@doctorbayter.com>
2021-12-15 08:44:04 -0500

    Agregar recetas dia 1 - desayuno  al reto navidad

--- routes/web.php
+++ routes/web.php
@@ -299,19 +299,19 @@ Route::get('x/query', function(){
 
 
 
-        // $recipe = Recipe::create([
-        //     'name' => 'Envueltillo en crema de toronjil',
-        //     'slug' => 'envueltillo-en-crema-de-toronjil',
-        //     'indice'=> 1,
-        //     'carbs' => 0,
-        //     'time' => 15,
-        //     'type' => 1,
-        // ]);
+        $recipe = Recipe::create([
+            'name' => 'Cerdín pingüen',
+            'slug' => 'cerdin-pinguen',
+            'indice'=> 1,
+            'carbs' => 11.39,
+            'time' => 25,
+            'type' => 1,
+        ]);
 
 
         $image = Image::create([
             'url' => 'recipes/aguaton.jpg',
-            'imageable_id' => 279,
+            'imageable_id' => $recipe->id,
             'imageable_type' => 'App\Models\Recipe',
         ]);
 
@@ -322,8 +322,35 @@ Route::get('x/query', function(){
             'videoable_type' => 'App\Models\Recipe',
         ]);*/
 
-        // DB::insert("INSERT INTO day_recipe (id, day_id, recipe_id, meal, created_at, updated_at) VALUES
-        // (280, 122, $recipe->id, '1', CURRENT_TIMESTAMP, NULL)");
+        DB::insert("INSERT INTO day_recipe (id, day_id, recipe_id, meal, created_at, updated_at) VALUES
+        (281, 122, $recipe->id, '1', CURRENT_TIMESTAMP, NULL)");
+
+
+$recipe = Recipe::create([
+    'name' => 'Canastilla de codorniz',
+    'slug' => 'canastilla-de-codorniz',
+    'indice'=> 1,
+    'carbs' => 0,
+    'time' => 15,
+    'type' => 1,
+]);
+
+
+$image = Image::create([
+    'url' => 'recipes/aguaton.jpg',
+    'imageable_id' => $recipe->id,
+    'imageable_type' => 'App\Models\Recipe',
+]);
+
+/*
+$video = Video::create([
+    'iframe' => '<iframe src="https://player.vimeo.com/video/638693117" class="w-full h-96" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen=""></iframe>',
+    'videoable_id' => $recipe->id,
+    'videoable_type' => 'App\Models\Recipe',
+]);*/
+
+DB::insert("INSERT INTO day_recipe (id, day_id, recipe_id, meal, created_at, updated_at) VALUES
+(282, 122, $recipe->id, '1', CURRENT_TIMESTAMP, NULL)");
 
 
 });

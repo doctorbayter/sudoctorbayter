b924ab8acc7449d1080b7c60b100f94492d91b46
jeffcote044 <web@doctorbayter.com>
2021-12-23 14:54:16 -0500

    Agregar registro desafio 2022

--- resources/views/livewire/reto/desafio/register.blade.php
+++ resources/views/livewire/reto/desafio/register.blade.php
@@ -7,11 +7,11 @@
             </div>
             <div class="pl-2 pr-2 mb-8 md:pl-12 md:pr-0 md:mb-0 ">
                 <header class="leading-tight">
-                    <p class="text-sm md:text-xl inline bg-red-700 text-gray-50 px-4"><b>Nuevo Reto</b> 100% Online</p>
+                    <p class="text-sm md:text-xl inline bg-red-700 text-gray-50 px-4"><b>Nuevo Desafio</b> 100% Online</p>
                     <h1 class="text-3xl md:text-5xl font-bold leading-none pt-2">Desafio 2022</h1>
                     <h2 class="text-base md:text-2xl">Libérate de la mierda del 2021</h2>
                 </header>
-                <p class="my-4 text-sm md:text-base text-justify">Porque este es tu año. Esta es tu oportunidad. Este 2022 vas a dejar de decir "el lunes comienzo la dieta" para decir "INICIO HOY". Y qué mejor que con un espectacular reto de 5 días en el que vas a eliminar toda la mierda que comiste en el 2021.</p>
+                <p class="my-4 text-sm md:text-base text-justify">Porque este es tu año. Esta es tu oportunidad. Este 2022 vas a dejar de decir "el lunes comienzo la dieta" para decir "INICIO HOY". Y qué mejor que con un espectacular Desafio de 5 días en el que vas a eliminar toda la mierda que comiste en el 2021.</p>
                 <p class="my-4 text-sm md:text-base text-justify">Vas a desintoxicar tu cuerpo, a romper tus adicciones por los carbohidratos y el azúcar, y lo mejor de todo, en tan solo 5 días: ¡VAS A BAJAR DE PESO!</p>
                 <p class="my-4 text-sm md:text-base text-justify">Prepárate porque durante 5 días te voy a llevar de la mano en un camino maravilloso en el que, con ingredientes económicos y deliciosos, probarás recetas increíbles que te desinflamarán, y sin aguantar hambre, <b>BAJARÁS DE PESO</b>.</p>
                 <div class="relative">
@@ -32,11 +32,11 @@
                 <img src="{{asset('img/billboards/banner_facts.png')}}" alt="" class="mx-auto">
             </div>
             <div class="text-gray-50  w-full lg:w-8/12  px-4 md:mr-0 md:ml-10">
-                <h2 class="font-bold mb-4 text-2xl leading-none">¿Qué incluye este reto?</h2>
+                <h2 class="font-bold mb-4 text-2xl leading-none">¿Qué incluye este Desafio?</h2>
                 <ul class="text-sm md:text-base">
                     <li class="mb-4 relative pl-6 text-justify">
                         <i class="far fa-check-square text-yellow-400 mr-2 absolute top-1 left-0"></i>
-                        <span class="">Menú de tu día a día durante los 5 días del reto.</span>
+                        <span class="">Menú de tu día a día durante los 5 días del Desafio.</span>
                     </li>
                      <li class="mb-4 relative pl-6 text-justify">
                         <i class="far fa-check-square text-yellow-400 mr-2 absolute top-1 left-0"></i>
@@ -50,7 +50,7 @@
                 </ul>
                 <div>
                     <hr class="my-6 block text-gray-700">
-                    <p class=" uppercase">Este RETO es <span class="font-bold">100% online </p>
+                    <p class=" uppercase">Este DESAFIO es <span class="font-bold">100% online </p>
                     <small>Estará disponible por tiempo limitado.</small>
                 </div>
             </div>
@@ -86,7 +86,7 @@
                 <h2 class="font-semibold text-3xl mb-4 leading-none">¿Cómo serán estos 5 días?</h2>
                 <h3 class="text-lg text-gray-200 leading-none"><b class="text-yellow-400">Fecha de inicio:</b> Martes 11 de enero 2022</h3>
             </div>
-            <p class="text-xl font-light mb-8">Esto es lo que verás en cada uno de los días del reto:</p>
+            <p class="text-xl font-light mb-8">Esto es lo que verás en cada uno de los días del desafio:</p>
             <ul>
                 <li class="mb-3 flex ">
                     <p class=""><b class="mr-4 text-yellow-400">Día 1:</b></p>
@@ -114,7 +114,7 @@
 
     <section class="max-w-4xl mx-auto">
         <div class="py-16">
-            <h2 class="font-bold mb-8 text-3xl text-center">¿Para quién es este reto?</h2>
+            <h2 class="font-bold mb-8 text-3xl text-center">¿Para quién es este desafio?</h2>
             <div class="grid lg:grid-cols-3 gap-x-6 gap-y-4">
                 <div class="bg-gray-100 px-6 py-4 rounded-lg shadow-sm">
                     <h2 class="font-bold text-xl my-4 text-red-700">Adultos</h2>
@@ -122,7 +122,7 @@
                 </div>
                 <div class="bg-gray-100 px-6 py-4 rounded-lg shadow-sm">
                     <h2 class="font-bold text-xl my-4 text-red-700">Jóvenes</h2>
-                    <p class="text-sm">Este es el reto perfecto para cambiar de hábitos y mejorar la salud.</p>
+                    <p class="text-sm">Este es el desafio perfecto para cambiar de hábitos y mejorar la salud.</p>
                 </div>
                 <div class="bg-gray-100 px-6 py-4 rounded-lg shadow-sm">
                     <h2 class="font-bold text-xl my-4 text-red-700">Parejas</h2>
@@ -144,7 +144,7 @@
             <ul>
                 <li class="mb-3 flex ">
                     <p class=""><b class="mr-4 text-yellow-400">1:</b></p>
-                    <p class=" flex-1 text-justify"><b> ¿Qué me llega al pagar? <br></b> Al momento del pago, te llegará un correo de confirmación para ingresar al grupo de WhatsApp. La información del reto la tendrás disponible el día viernes 7 de enero en tu cuenta. Para acceder a esta, debes iniciar sesión en doctorbayter.com con el correo y contraseña que registraste cuando realizaste el pago.</p>
+                    <p class=" flex-1 text-justify"><b> ¿Qué me llega al pagar? <br></b> Al momento del pago, te llegará un correo de confirmación para ingresar al grupo de WhatsApp. La información del desafio la tendrás disponible el día viernes 7 de enero en tu cuenta. Para acceder a esta, debes iniciar sesión en doctorbayter.com con el correo y contraseña que registraste cuando realizaste el pago.</p>
                 </li>
                 <li class="mb-3 flex">
                     <p class=""><b class="mr-4 text-yellow-400">2:</b></p>

9d7dc05b6633751e1f60e7e8559393e7f3e29a7e
jeffcote044 <web@doctorbayter.com>
2022-06-07 13:30:56 -0500

    Nuevo material

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,10 @@ Route::get('libro/', function(){
     return redirect()->away("https://www.amazon.com/-/es/dp/B09V1PD9VC/ref=sr_1_1?crid=2KFPRGHJW9WD&keywords=catastrofes+en+cirugia+plastica&qid=1646685263&s=digital-text&sprefix=%2Cdigital-text%2C161&sr=1-1");
 });
 
+Route::get('pareja/', function(){
+    return redirect()->away("https://biz.payulatam.com/L0bdc050469E638");
+});
+
 Route::get('/privacidad', function(){
     return view('terms');
 } )->name('privacidad');

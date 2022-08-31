c0a7b1e78878060c10873d398ea0ff4b01382052
jeffcote044 <web@doctorbayter.com>
2022-08-31 14:47:16 -0500

    Cierre venta reto 5Mer

--- app/Http/Livewire/ProductPay.php
+++ app/Http/Livewire/ProductPay.php
@@ -54,7 +54,7 @@ class ProductPay extends Component
 
     public function render() {
 
-        if(  $this->plan->id == 18 || $this->plan->id == 19 || $this->plan->id == 25 || $this->plan->id == 36){
+        if( $this->plan->id == 15 ||  $this->plan->id == 18 || $this->plan->id == 19 || $this->plan->id == 25 || $this->plan->id == 36){
             return view('no-disponible-lw');
         }
             return view('livewire.product-pay');

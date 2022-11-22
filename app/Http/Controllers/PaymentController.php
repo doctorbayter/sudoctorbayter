5c9e78f750744e6696c7e39a06b1b597fdfa0c52
jeffcote044 <web@doctorbayter.com>
2022-11-22 18:55:01 -0500

    WH Hotmart

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -383,7 +383,7 @@ class PaymentController extends Controller
         $user = User::find(3523);
         $fase = Fase::find(14);
         $fase->clients()->attach($user->id);
-        $data = response()->json($request->all());
+        //$data = response()->json($request->all());
         //$id = $data['data']['product']['id'];
         //$email = $data['data']['buyer']['email'];
         //$status = $data['status'];
@@ -391,6 +391,6 @@ class PaymentController extends Controller
         //$phone = $data['phone_checkout_local_code'];
         //$buyer = $data['name'];
 
-        return $data;
+        return json_decode($request->getContent(), true);
     }
 }

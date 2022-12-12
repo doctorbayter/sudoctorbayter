e877a7ea7c2e21d86db8df22a295f6dde85c1589
jeffcote044 <web@doctorbayter.com>
2022-12-11 22:26:42 -0500

    Nuevo material

--- resources/views/livewire/user-fase.blade.php
+++ resources/views/livewire/user-fase.blade.php
@@ -116,10 +116,14 @@
 
                                     @foreach ($this->day->recipes->where('type', '==', 1)->sortBy('pivot.meal') as $key => $recipe)
 
+
                                         <div class="pt-12 md:pt-0">
                                             <a href="{{route('plan.recipe', $recipe)}}">
                                                 <div class="relative h-62">
+
                                                     <div class="absolute text-sm bg-gray-100 bottom-full px-4 py-2 font-bold leading-none text-gray-900  rounded-t-lg ml-2 ">
+
+                                                        {{$recipe->days[0]->pivot}}
 
                                                         @switch($recipe->days[0]->pivot->meal)
                                                             @case(1)

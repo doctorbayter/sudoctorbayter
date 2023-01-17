ea7c325f8a87a06c356be6ae87eef0a36e6aec8c
sudoctorbayter <web@doctorbayter.com>
2023-01-17 00:41:35 -0500

    Desafio 2023 Oferta Cierre

--- routes/web.php
+++ routes/web.php
@@ -155,13 +155,13 @@ Route::get('/99', function () {
 })->name('reto.oferta');
 
 Route::get('/67', function () {
-    //return view('no-disponible');
+    return view('no-disponible');
     $plan = Plan::find(15);
     return redirect()->route('payment.pay', ['plan'=>$plan]);
 })->name('reto.oferta');
 
 Route::get('/oferta', function () {
-    //return view('no-disponible'); 
+    return view('no-disponible'); 
     return redirect('https://pay.hotmart.com/F78337495Q?off=7eyky1c2&checkoutMode=10');
 })->name('desafio.2023');
 

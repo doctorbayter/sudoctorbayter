b1fcc8010c8b5a37b50e93feb1bf7591ac329217
sudoctorbayter <web@doctorbayter.com>
2023-01-18 17:46:42 -0500

    Metodo DKP Hotmart

--- resources/views/dkp.blade.php
+++ resources/views/dkp.blade.php
@@ -67,7 +67,7 @@
                             @endif
                         </div>
 
-                        <a href="#go" class=" inline-block mt-2 font-bold px-4 py-2 rounded-lg border bg-red-700 border-red-700 text-white uppercase transition-colors duration-300 ease-in-out text-lg hover:bg-transparent hover:text-red-700">¡Adquiere tu plan desde 75 US$ !</a>
+                        <a href="#go" class=" inline-block mt-2 font-bold px-4 py-2 rounded-lg border bg-red-700 border-red-700 text-white uppercase transition-colors duration-300 ease-in-out text-lg hover:bg-transparent hover:text-red-700">¡Adquiere el Método Ya!</a>
                     @endauth
 
                 </header>

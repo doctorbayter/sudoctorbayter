c24456499594298f7fdebbfa5d71c5d8f960245a
sudoctorbayter <web@doctorbayter.com>
2023-02-10 15:46:03 -0500

    Google Tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,15 +26,13 @@
         <!-- Scripts -->
         <script src="{{ mix('js/app.js') }}" defer></script>
 
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-8XKP69B28G"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-8XKP69B28G');
-        </script>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-58QC4WL');</script>
+        <!-- End Google Tag Manager -->
 
         <!-- Facebook Pixel Code -->
         <script>
@@ -65,6 +63,11 @@
 --}}
     </head>
     <body class="font-sans antialiased">
+
+     <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-58QC4WL"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
 
 {{--
     intl-tel-input CODE

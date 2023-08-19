06874ab8978a4d6b5fb0ca0409bd485208b5fb4a
sudoctorbayter <web@doctorbayter.com>
2023-08-19 10:35:33 -0500

    ARREGLOS FASE 1 AVISO

--- routes/web.php
+++ routes/web.php
@@ -75,6 +75,10 @@ Route::get('libro/', function(){
     return redirect()->away("https://www.amazon.com/-/es/dp/B09V1PD9VC/ref=sr_1_1?crid=2KFPRGHJW9WD&keywords=catastrofes+en+cirugia+plastica&qid=1646685263&s=digital-text&sprefix=%2Cdigital-text%2C161&sr=1-1");
 });
 
+Route::get('librodkp/', function(){
+    return redirect()->away("https://www.amazon.com/-/es/Jorge-Enrique-Bayter-Marín-ebook/dp/B0BHBZNMGP");
+});
+
 Route::get('miami/', function(){
     return redirect()->away("https://www.eventbrite.com/e/mas-alla-de-la-victoria-con-doctor-bayter-junto-drpedrito-tickets-378889818647");
 });

40ff03ca04977fd2596054189d56655170cae0bf
sudoctorbayter <web@doctorbayter.com>
2023-09-01 12:47:34 -0500

    RETO 5MER 2023 Configuracion Venta v3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,8 +55,8 @@
         }
 }
     </style>
-    <article class="text-center mt-12 md:mt-24 mx-auto max-w-5xl px-2 hidden">
-        <a href="https://pay.hotmart.com/S83188903T?checkoutMode=10" target="_blank"><img src="{{asset('img/billboards/empareja2_2023_banner.png')}}" alt="" class="w-full mx-auto shadow-xl overflow-hidden rounded-xl animate-wiggle"></a>
+    <article class="text-center mt-12 md:mt-24 mx-auto max-w-5xl px-2">
+        <a href="https://pay.hotmart.com/B86320853L?checkoutMode=10" target="_blank"><img src="{{asset('img/billboards/banner_5mer2023.png')}}" alt="" class="w-full mx-auto shadow-xl overflow-hidden rounded-xl animate-wiggle"></a>
     </article>
 
     <article class="text-center mt-12 md:mt-24 mx-auto max-w-5xl px-2 hidden">

f8119a3eeec99c6a2c7f65414b44f470fc4cabbd
sudoctorbayter <web@doctorbayter.com>
2023-09-14 19:04:24 -0500

    RETO 5MER 2023 Reunion Repeticion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,7 +55,7 @@
         }
 }
     </style>
-    <article class="text-center mt-12 md:mt-24 mx-auto max-w-5xl px-2">
+    <article class="text-center mt-12 md:mt-24 mx-auto max-w-5xl px-2 hidden">
         <a href="https://pay.hotmart.com/B86320853L?checkoutMode=10" target="_blank"><img src="{{asset('img/billboards/banner_5mer2023.png')}}" alt="" class="w-full mx-auto shadow-xl overflow-hidden rounded-xl animate-wiggle"></a>
     </article>
 

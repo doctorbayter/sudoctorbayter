95bc9397881d16343241e43f758c61b73fd76edf
sudoctorbayter <web@doctorbayter.com>
2023-10-24 23:26:11 -0500

    RECETAS F2 VIDEOS

--- routes/web.php
+++ routes/web.php
@@ -78,7 +78,7 @@ Route::get('libro/', function(){
 Route::get('librodkp/', function(){
     return redirect()->away("https://www.amazon.com/-/es/Jorge-Enrique-Bayter-Marín-ebook/dp/B0BHBZNMGP");
 });
- 
+  
 Route::get('librocomerparasanar/', function(){
     return redirect()->away("https://www.buscalibre.com.co/libro-comer-para-sanar-doctor-bayter-editorial-planeta/9786287665101/p/55573793");
 });

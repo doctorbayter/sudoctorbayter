47b85b6d509b3da8471bad4fe4a995fcf4e63424
sudoctorbayter <web@doctorbayter.com>
2023-10-24 23:17:58 -0500

    RECETAS F2 VIDEOS

--- app/Http/Livewire/UserPlan.php
+++ app/Http/Livewire/UserPlan.php
@@ -57,7 +57,7 @@ class UserPlan extends Component
         $this->subscribed_fase_week = $fase_week->clients->contains(auth()->user()->id);
 
 
-        $reto_actual = Fase::find(17);
+        $reto_actual = Fase::find(1700);
         $this->subscribed_reto_actual = $reto_actual->clients->contains(auth()->user()->id);
 
         $planUpdate = Plan::find(3);

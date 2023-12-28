14b35a71212b5a0557eb5870af845a0a19c7071f
sudoctorbayter <web@doctorbayter.com>
2023-12-27 19:54:26 -0500

    Link Yoanita

--- resources/views/livewire/user-fase.blade.php
+++ resources/views/livewire/user-fase.blade.php
@@ -34,7 +34,7 @@
                                         <div class="flex flex-col ml-3">
                                             <div class=" leading-none font-bold text-red-700">¡Aviso Importante!</div>
                                             <p class="text-sm text-gray-600 leading-none mt-1">El reto <b>En Nevidad 2023</b> Dará inicio el <b>9 de Enero de 2024</b></p>
-                                            <p class="text-sm text-gray-600 leading-none mt-1">por ahora puedes ingresar a <a class="text-white text-sm border cursor-pointer border-red-700 bg-red-700 hover:text-red-800 hover:bg-white inline-block font-bold px-4 py-2 rounded-full" href="https://doctorbayter.com/reto/navidad/whatsapp" target="_blank" rel="noopener noreferrer"><b>El grupo de Whatsapp</b></a> Dirigido por <b>El Equipo de Tu Doctor Bayter</b></p>
+                                            <p class="text-sm text-gray-600 leading-none mt-1">por ahora puedes ingresar a <a class="text-white text-sm border cursor-pointer border-red-700 bg-red-700 hover:text-red-800 hover:bg-white inline-block font-bold px-4 py-2 rounded-full" href="https://doctorbayter.com/reto/desafio/whatsapp" target="_blank" rel="noopener noreferrer"><b>El grupo de Whatsapp</b></a> Dirigido por <b>El Equipo de Tu Doctor Bayter</b></p>
                                             <p class="text-sm text-gray-600 leading-none mt-1">Recurda que: <b>Toda la información del reto quedará activa el día viernes 5 de Enero de 2024 (Recetas, Lista de alimentos y Secretos)</b></p>
                                         </div>
                                     </div>

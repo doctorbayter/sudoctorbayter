56c5247b572442a434e50bc7bf6997d92fd7460e
sudoctorbayter <web@doctorbayter.com>
2024-01-05 20:03:59 -0500

    Reto 2024 activacion #1

--- app/Http/Controllers/HotmartController.php
+++ app/Http/Controllers/HotmartController.php
@@ -38,7 +38,7 @@ class HotmartController extends Controller
                 // Lógica para verificar con la API de Hotmart y activar el usuario
                 $response = $this->hotmartService->getCustomerProduct($productId , $user->email);
                 
-                if (!empty($response) && isset($response['items']) && $response['items'] > 0) {
+                if (!empty($response) && !empty($response['items'])) {
                     $buyerInfo = $this->buyerInfo($response);
                     $this->sendReto($buyerInfo);
                     echo $buyerInfo['email'] . " (Activado) </br>";
